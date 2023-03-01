028e25802cdadc5b3940fbd7b6389ca7da3cf707
Alex Maldonado <alex.maldonado@outlook.com>
2023-03-01 09:11:14 -0300

    Added first class

---
+++ src/01-class.ts
@@ -0,0 +1,22 @@
+class MyDate {
+
+  year:number;
+  month:number;
+  day:number;
+
+  constructor(
+    year: number,
+    month: number,
+    day: number
+  ) {
+
+    this.year  = year;
+    this.month = month;
+    this.day   = day;
+
+  }
+
+}
+
+const myDate = new MyDate(2023, 3, 1);
+console.log(myDate);
